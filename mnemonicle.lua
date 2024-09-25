c3ac57d1aa32aa1be32b9067b7da506343e68073
FlameFish <mccoa499@gmail.com>
2024-09-25 16:17:11 -0700

    Added modes, removed function test()

--- mnemonicle.lua
+++ mnemonicle.lua
@@ -5,14 +5,26 @@ mnemonicle.dir = "/mnemonicle"
 local globalFilePath = mnemonicle.dir .. "/global.json"
 local localFilePath = mnemonicle.dir .. "/local.json"
 
-function mnemonicle.init()
+mnemonicle.modes = {
+    LOCAL = {localEnabled = true, globalEnabled = false}, -- Relies fully on local navigation, does not use global at all.
+    GLOBAL = {localEnabled = false, globalEnabled = true}, -- Relies soley on global navigation, requires GPS at all times. Not reccomended.
+    HYBRID = {localEnabled = true, globalEnabled = true} -- Uses GPS when available. Otherwise, uses local navigation. Reccomended.
+}
+
+function mnemonicle.init(mode)
+    mnemonicle.mode = mode or mnemonicle.modes.HYBRID
+    if not mnemonicle.modes[mnemonicle.mode] then
+        error("Invalid mode! Valid modes are mnemonicle.LOCAL, mnemonicle.GLOBAL, and mnemonicle.HYBRID.",2)
+    end
     globalFilePath = mnemonicle.dir .. "/global.json"
     localFilePath = mnemonicle.dir .. "/local.json"
 
-    mnemonicle.loadGlobals()
-    mnemonicle.loadLocals()
-    mnemonicle.writeGlobals()
-    mnemonicle.writeLocals()
+    if mnemonicle.mode.localEnabled then
+        mnemonicle.loadGlobals()
+    end
+    if mnemonicle.mode.globalEnabled then
+        mnemonicle.loadLocals()
+    end
 end
 
 mnemonicle.globalVars = {
@@ -69,8 +81,10 @@ function mnemonicle.writeLocals()
     file.close()
 end
 
-function mnemonicle.test()
-    print("test")
-end
+-- Calculations
+
+
+-- Movement Functions
+
 
 return mnemonicle
