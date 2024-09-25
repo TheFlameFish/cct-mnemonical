ffd59e68f561dfaa21ac83ca3bacf89b50624f3d
FlameFish <mccoa499@gmail.com>
2024-09-25 13:35:01 -0700

    Fixed mispelling in array mnemonical → mnemonicle

--- mnemonicle.lua
+++ mnemonicle.lua
@@ -1,21 +1,21 @@
-local mnemonical = {}
+local mnemonicle = {}
 
-mnemonical.dir = "/mnemonicle"
+mnemonicle.dir = "/mnemonicle"
 
-local globalFilePath = mnemonical.dir .. "/global.json"
-local localFilePath = mnemonical.dir .. "/local.json"
+local globalFilePath = mnemonicle.dir .. "/global.json"
+local localFilePath = mnemonicle.dir .. "/local.json"
 
-function mnemonical.init()
-    globalFilePath = mnemonical.dir .. "/global.json"
-    localFilePath = mnemonical.dir .. "/local.json"
+function mnemonicle.init()
+    globalFilePath = mnemonicle.dir .. "/global.json"
+    localFilePath = mnemonicle.dir .. "/local.json"
 
-    mnemonical.loadGlobals()
-    mnemonical.loadLocals()
-    mnemonical.writeGlobals()
-    mnemonical.writeLocals()
+    mnemonicle.loadGlobals()
+    mnemonicle.loadLocals()
+    mnemonicle.writeGlobals()
+    mnemonicle.writeLocals()
 end
 
-mnemonical.globalVars = {
+mnemonicle.globalVars = {
     position = {
         x = 0,
         y = 0,
@@ -27,7 +27,7 @@ mnemonical.globalVars = {
         z = 0
     }
 }
-mnemonical.localVars = {
+mnemonicle.localVars = {
     position = {
         x = 0,
         y = 0,
@@ -35,42 +35,42 @@ mnemonical.localVars = {
     }
 }
 
-function mnemonical.loadGlobals()
+function mnemonicle.loadGlobals()
     if fs.exists(globalFilePath) then
         local file = fs.open(globalFilePath,"r")
         local contents = file.readAll()
         file.close()
 
-        mnemonical.globalVars = textutils.unserializeJSON(contents)
+        mnemonicle.globalVars = textutils.unserializeJSON(contents)
     end
-    return mnemonical.globalVars
+    return mnemonicle.globalVars
 end
 
-function mnemonical.loadLocals()
+function mnemonicle.loadLocals()
     if fs.exists(localFilePath) then
         local file = fs.open(localFilePath,"r")
         local contents = file.readAll()
         file.close()
 
-        mnemonical.localVars = textutils.unserializeJSON(contents)
+        mnemonicle.localVars = textutils.unserializeJSON(contents)
     end
-    return mnemonical.localVars
+    return mnemonicle.localVars
 end
 
-function mnemonical.writeGlobals()
+function mnemonicle.writeGlobals()
     local file = fs.open(globalFilePath,"w")
-    file.write(textutils.serialiseJSON(mnemonical.globalVars))
+    file.write(textutils.serialiseJSON(mnemonicle.globalVars))
     file.close()
 end
 
-function mnemonical.writeLocals()
+function mnemonicle.writeLocals()
     local file = fs.open(localFilePath,"w")
-    file.write(textutils.serialiseJSON(mnemonical.localVars))
+    file.write(textutils.serialiseJSON(mnemonicle.localVars))
     file.close()
 end
 
-function mnemonical.test()
+function mnemonicle.test()
     print("test")
 end
 
-return mnemonical
+return mnemonicle
